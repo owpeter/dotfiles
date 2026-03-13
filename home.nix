2a4046ef0546c00afa7db1d9bf7f2156835d72c4
Kie-Chi <137579437@qq.com>
2026-03-13 16:06:46 +0800

    fix(fcitx5): fix fcitx5 in wayland.

--- home.nix
+++ home.nix
@@ -22,6 +22,13 @@ in
       x11.enable = true;
       gtk.enable = true;
     };
+    home.sessionVariables = {
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+      SDL_IM_MODULE = "fcitx";
+      GLFW_IM_MODULE = "ibus";
+    };
     _module.args.isDesktop = isDesktop;
     programs.home-manager.enable = true;
   };
